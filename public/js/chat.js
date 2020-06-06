fb5ccd796f1e1490b40d1ce0031161b9ba7ebd63
Arafat Najjashi <arafath.moghul@gmail.com>
2020-06-06 20:27:24 +0530

    server and multiple client connection

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,9 +1,20 @@
  const socket = io()
 
- socket.on('countUpdated', (count) => {
-     console.log('count has been updated!', count)
- })
+//  socket.on('countUpdated', (count) => {
+//      console.log('count has been updated!', count)
+//  })
 
- document.querySelector('#increment').addEventListener('click', () => {
-     socket.emit('increment')
- })
+//  document.querySelector('#increment').addEventListener('click', () => {
+//      socket.emit('increment')
+//  })
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit',(e)=> {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage',message)
+})
